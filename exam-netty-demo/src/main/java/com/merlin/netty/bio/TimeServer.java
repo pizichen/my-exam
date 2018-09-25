c027e1c917dd69bcdac0fe195de56113274ab494
merlin chen <18908086599@163.com>
2018-09-25 16:10:11 +0800

    bio test codes commit

---
+++ exam-netty-demo/src/main/java/com/merlin/netty/bio/TimeServer.java
@@ -0,0 +1,38 @@
+package com.merlin.netty.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+
+    public static void main(String[] args) throws IOException{
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+//                采用默认值
+            }
+        }
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("The time server is start in port:" + port);
+            Socket socket = null;
+            while (true) {
+                socket = serverSocket.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (serverSocket != null) {
+                System.out.println("The time server close");
+                serverSocket.close();
+                serverSocket = null;
+            }
+        }
+    }
+}
